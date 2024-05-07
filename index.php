838e951410f68c28178fb30f487cd98351245ba6
Gian Carlo S. Victorino <155649616+giancarlo0326@users.noreply.github.com>
2024-05-08 00:05:52 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                <img src="https://i.postimg.cc/h42Dh6mx/The-National-Ledger-5-7-2024-1.png" class="header-img" alt="Logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="balance.php">Balance</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="deposit.php">Deposit</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="withdraw.php">Withdraw</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-lg-12">
+            <div class="centered-container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div id="image-container">
+                            <img src="https://i.postimg.cc/L6ssT2SN/Your-account-is-our-National-P-5-7-2024.png" class="img-fluid">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div id="image-container">
+                            <img src="https://i.postimg.cc/bwfTKCvD/1693421720934.jpg" class="img-fluid" alt="Responsive image">
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+    
+    <div id="image-container">
+        <img src="https://i.postimg.cc/kM21tYYz/We-do-things-the-National-Way-5-7-2024.png" class="img-fluid">
+    </div>
+
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
